e0d973caa129516ee00a608f14986ec12a2ef2ff
TrieuPhan <trieuphan@rubikintegration.com>
2012-04-25 14:10:06 +0700

    added zencart files

---
+++ includes/templates/stelis/template_info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Information File
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
+ */
+$template_name = 'Stelis Template';
+$template_version = 'Version 1.0';
+$template_author = 'Rubik Integration Team (c) 2012';
+$template_description = '...';
+$template_screenshot = 'scr_template_default.jpg';
